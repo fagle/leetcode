e48176daa9505bff9e2817128e033be0f070bb25
fagle <ifagle@qq.com>
2017-01-13 23:03:47 +0800

    working

--- 321_CreateMaximunNumber.cc
+++ 321_CreateMaximunNumber.cc
@@ -5,17 +5,59 @@ using namespace std;
 
 class Solution {
 public:
+
+    struct Node {
+        Node(d, t): data(d), type(t)  {}
+        int data;
+        int type;
+    };   
+
+
+    class MaxPQ {
+    private:
+        vector<Node> pq;
+    public:
+        MaxPQ(int k): pq(k) {}
+        void insert(Node& node) {
+            for (int i=0; i< pq.size(); i++) {
+                if (type == 1) {
+                } else {
+                }
+            }
+        }
+
+        int delMax() {
+            return 0;
+        }
+
+        void swap(int i, int j) {
+        }
+
+
+
+    }
+
     vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<int> pq(nums2);
+        pq.clear(); 
+
         cout << "pq.size = " << pq.size() << endl;
         for(int i=0; i<pq.size(); i++) {
             cout << "pq["<< i <<"] = " <<pq[i] << endl;
         }
-        while(false) {
-            
+        int i1=0, i2=0;
+        while(true) {
+            if (nums1.size()>0) {
+                Node node(nums1[0], 1);
+                insert(node);
+            } 
+            if (nums2.size()>0) {
+                Node node(nums2[0], 2);
+                insert(node);
+            }
         }
         return pq;
     }
+    
 };
 
 int main() {
