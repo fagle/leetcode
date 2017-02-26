35e8384104a7b15d06312f0d5997de57694f6218
frost <ifagle@qq.com>
2017-02-26 22:33:15 +0800

    怎么又漏了一个，我去。。

--- mysql/solutions.sql
+++ mysql/solutions.sql
@@ -1,3 +1,8 @@
+
+/* 175. Combine Two Tables */
+/* Runtime: 755 ms */
 select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId;
 
+/* 176. Second Highest Salary */
+/* Runtime: 1681 ms */
 select max(Salary) as SecondHighestSalary from Employee where Salary != (select max(Salary) from Employee);
