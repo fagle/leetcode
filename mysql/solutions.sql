83bfd44c34f7e17ba1e7646d9d72366cb1959d14
frost <ifagle@qq.com>
2017-03-03 20:41:59 +0800

    solved sql problem 181

--- mysql/solutions.sql
+++ mysql/solutions.sql
@@ -9,7 +9,18 @@ select max(Salary) as SecondHighestSalary from Employee where Salary != (select 
 
 /* beats 43.98% submissions*/
 /* Runtime: 867 ms */
+CREATE TABLE Employee (Id INT , Salary INT );
+INSERT INTO Employee VALUES (1, 100);
+INSERT INTO Employee VALUES (2, 200),(3, 300);
 select max(Salary) as SecondHighestSalary from Employee where Salary != (select max(Salary) from Employee where Salary >= (select AVG (Salary) from Employee));
+
+/* beats 41.27% submissions*/
+/* Runtime: 898 ms */
+SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Id IN (SELECT a.Id FROM Employee a, Employee b WHERE a.Salary < b.Salary);
+
+/* beats 40.07% submissions*/
+/* Runtime: 909 ms */
+SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Id IN (SELECT a.Id FROM Employee a WHERE a.Salary < (SELECT max(Salary) FROM Employee) and a.Salary >= (SELECT AVG(Salary) FROM  Employee) );
 
 /* 196. Delete Duplicate Emails */
 /* Runtime: 971 ms */
@@ -23,3 +34,11 @@ CREATE TABLE Person (Id int, Email VARCHAR (50));
 INSERT INTO Person VALUES (1, 'd@wori.me'),(2,'d@wori.me');
 INSERT INTO Person VALUES (3, 'ifagle@qq.com');
 DELETE a FROM Person a, Person b WHERE a.Email=b.Email AND a.Id>b.Id;
+
+/* 181. Employees Earning More Than Their Managers */
+/* Runtime: 988 ms */
+/* beats 92.20% submissions*/
+drop table if EXISTS Employee;
+CREATE TABLE Employee (Id INT, Name VARCHAR(20), Salary INT , ManagerId INT );
+insert into Employee values(1, 'joe', 70000, 3), (2, 'henry', 80000, 4), (3, 'sam', 60000, NULL ), (4, 'Max', 90000, NULL );
+select a.Name as  Employee from Employee a, Employee b where  a.ManagerId = b.Id and a.Salary > b.Salary;
