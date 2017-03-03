7330c170281c75e73f89ad9009e88e683a141def
frost <ifagle@qq.com>
2017-03-03 21:11:52 +0800

    183. Customers Who Never Order

--- mysql/solutions.sql
+++ mysql/solutions.sql
@@ -48,3 +48,12 @@ select a.Name as  Employee from Employee a, Employee b where  a.ManagerId = b.Id
 /* beats 56.56% submissions*/
 INSERT INTO Person VALUES (2,'d@wori.me');
 select DISTINCT(a.Email) from Person a, Person b where  a.Id != b.Id and a.Email = b.Email;
+
+/* 183. Customers Who Never Order */
+/* Runtime: 615 ms */
+/* beats 44.67% submissions*/
+create table Customers (Id int, Name varchar(20));
+create table Orders(Id int, CustomerId int);
+insert into Customers values(1, 'joe'), (2, 'henry'), (3, 'sam'), (4, 'Max');
+insert into Orders values(1, 3), (2,1);
+select Name from Customers where Id not in (select Customers.Id from Customers JOIN Orders where Customers.Id = Orders.CustomerId);
