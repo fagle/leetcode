a74fc0799ea9bc9cd27af020d09105958f86396d
邓团飞 <ifagle@qq.com>
2017-03-22 10:08:35 +0800

    thinking in sql

--- mysql/solutions.sql
+++ mysql/solutions.sql
@@ -99,3 +99,80 @@ GROUP BY sid;
 
 /* 查询所有区最高在线人数情况 */
 SELECT sum(online_num) as max_num, record_time FROM onlineLog GROUP BY record_time ORDER BY max_num DESC LIMIT  10;
+
+/* 最近在线人数, 5.115s */
+SELECT
+  sum(online_num) AS current_num,
+  record_time
+FROM
+  onlineLog
+WHERE
+  record_time IN (SELECT DISTINCT * FROM (
+    SELECT
+      record_time
+    FROM
+      onlinelog
+    ORDER BY
+      record_time DESC
+    LIMIT 10
+  ) as a)
+GROUP BY
+  record_time;
+
+/* 最近在线人数，方法2, 3.729s */
+SELECT
+  sum(b.online_num) AS current_num,
+  a.record_time
+FROM
+  onlineLog b,
+  (
+    SELECT
+      max(record_time) AS record_time
+    FROM
+      onlineLog
+  ) AS a
+WHERE
+  b.record_time = a.record_time
+GROUP BY
+  a.record_time;
+
+/* 最近10次在线人数统计,14.816s */
+SELECT
+  sum(online_num) AS current_num,
+  record_time
+FROM
+  onlineLog
+WHERE
+  record_time IN (
+    SELECT DISTINCT
+      *
+    FROM
+      (
+        SELECT DISTINCT
+          record_time
+        FROM
+          onlinelog
+        ORDER BY
+          record_time DESC
+        LIMIT 10
+      ) AS a
+  )
+GROUP BY
+  record_time;
+
+/* 最近10次在线人数，方法2, 17.722s */
+SELECT
+  sum(b.online_num) AS current_num,
+  a.record_time
+FROM
+  onlineLog b,
+  (
+    SELECT
+      DISTINCT record_time AS record_time
+    FROM
+      onlineLog ORDER BY record_time DESC LIMIT 10
+  ) AS a
+WHERE
+  b.record_time = a.record_time
+GROUP BY
+  a.record_time;
