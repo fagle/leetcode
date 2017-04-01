e7823cabe74fa6e695ef3725a3a768e8b831e61e
邓团飞 <ifagle@qq.com>
2017-04-01 17:47:55 +0800

    Time Limit Exceeded (⊙o⊙)… 5. Longest Palindromic Substring

--- src/main/java/me/nubi/leetcode/Problem005.java
+++ src/main/java/me/nubi/leetcode/Problem005.java
@@ -3,29 +3,51 @@ package me.nubi.leetcode;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Queue;
-import java.util.Stack;
-import java.util.concurrent.LinkedBlockingQueue;
-
 /**
  * Created by fagle on 17-3-31.
  * 5. Longest Palindromic Substring
  */
 public class Problem005 {
     public class Solution {
+
+        boolean isPalindrome(String s) {
+            for (int i=0; i<s.length(); i++) {
+                if (s.charAt(i) != s.charAt(s.length() - i - 1))
+                    return false;
+            }
+            return true;
+        }
+
         public String longestPalindrome(String s) {
-            Queue<Character> queue = new LinkedBlockingQueue<>();
-            Stack<Character> stack;
-            return null;
+            String maxstr = s.substring(0, 1);
+            for (int i=0; i<s.length(); i++) {
+               for (int j=i+1; j<s.length(); j++) {
+                   if (j - i + 1 <= maxstr.length())
+                       continue;
+                   String tmp = s.substring(i, j+1);
+                   if (isPalindrome(tmp))
+                       maxstr = tmp;
+               }
+            }
+            return maxstr;
         }
     }
 
     @Test
     public void test() {
+        long start = System.currentTimeMillis();
         String s = "babad";
         Solution solution = new Solution();
         Assert.assertEquals("bab", solution.longestPalindrome(s));
         s = "cbbd";
         Assert.assertEquals("bb", solution.longestPalindrome(s));
+        s = "a";
+        Assert.assertEquals("a", solution.longestPalindrome(s));
+        s = "rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip";
+        Assert.assertEquals("qgjjgq", solution.longestPalindrome(s));
+        s = "anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg";
+        Assert.assertEquals("hpyyph", solution.longestPalindrome(s));
+        long end = System.currentTimeMillis();
+        System.out.printf("spend time: %dms.\n", end - start);
     }
 }
