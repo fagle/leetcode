ef933c6a281d4a7d05ca2f02d5bdf7c2041962a6
fitz <ifagle@qq.com>
2026-05-26 01:20:39 +0800

    对称二叉树递归解法

---
+++ src/main/java/me/nubi/leetcode/q101/Solution2.java
@@ -0,0 +1,28 @@
+package me.nubi.leetcode.q101;
+
+import me.nubi.leetcode.type.TreeNode;
+
+class Solution2 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null) {
+            return false;
+        }
+        if (right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
